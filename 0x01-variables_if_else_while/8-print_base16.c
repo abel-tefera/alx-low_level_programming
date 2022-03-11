976166e0c241f91ccd7a996e8306a05c8fd17662
Abel <abeltefera16@gmail.com>
2022-03-11 12:22:04 +0300

    betty fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,4 @@
-
 #include <stdio.h>
-
 /**
  * main - Entry point
  * Return:0
@@ -8,15 +6,17 @@
 int main(void)
 {
 	int i, y;
+
 	for (i = 0; i < 10; i++)
 	{
 		putchar (i + '0');
 	}
+
 	for (y = 'a'; y <= 'f'; y++)
 	{
 		putchar (y);
 	}
 
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
